b8b30f3726a7dbe4d8afa02070c03e1c3a1336b9
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2024-12-14 22:26:15 +0000

    build

--- cmd/serv/main.go
+++ cmd/serv/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"os"
 
 	"github.com/KVRes/District/serv"
 	"github.com/KevinZonda/GoX/pkg/iox"
 )
 
 type Config struct {
-	Protocol string `json:"protocol"`
-	Addr     string `json:"addr"`
+	Protocol string  `json:"protocol"`
+	Addr     string  `json:"addr"`
+	HA       *string `json:"ha"`
+}
+
+func (c Config) GetHA() string {
+	if c.HA == nil {
+		return "data"
+	}
+	return *c.HA
 }
 
 var cfg Config
@@ -30,17 +40,27 @@ func normalizeConfig() {
 func loadConfig() {
 	bs, err := iox.ReadAllByte("config.json")
 	if err != nil {
-		panic(err)
+		log.Println("no config file, use default one")
+		goto end
 	}
 	if err := json.Unmarshal(bs, &cfg); err != nil {
 		panic(err)
 	}
+end:
 	normalizeConfig()
 }
 
 func main() {
 	loadConfig()
+	ha := cfg.GetHA()
+	err := os.MkdirAll(ha, 0755)
+	log.Println("create ha dir", ha)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
 	svr := serv.NewServer()
+
+	log.Println("start server at:", cfg.Protocol, cfg.Addr)
 
 	svr.Run(cfg.Protocol, cfg.Addr)
 }
